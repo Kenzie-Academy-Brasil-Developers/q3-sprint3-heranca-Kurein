60ff5f695c94d53dd909feec78075dc6d161914e
Kaueh <kaueh1234@gmail.com>
2022-01-18 11:30:24 -0300

    Entrega finalizada

--- classes/copo.py
+++ classes/copo.py
@@ -1,1 +1,34 @@
-# Desenvolva sua classe Copo aqui.
+# -*- coding: utf-8 -*-
+
+from recipiente import Recipiente
+
+class Copo(Recipiente):
+    def __init__(self, tamanho, conteudo, limpo):
+        super().__init__(tamanho, conteudo, limpo)
+
+    def encher(self, bebida = 'água'):
+        if self.esta_limpo:
+            self.sujar()
+            self.conteudo = self.tamanho
+            self.bebida = bebida
+        else:
+            print('Não se pode encher um copo sujo!')
+
+    def beber(self, quantidade):
+        if quantidade < 0:
+            print('Quantidade deve ser positiva')
+        elif quantidade > self.conteudo:
+            print('Não há bebida suficiente no copo')
+        else:
+            self.conteudo -= quantidade
+        
+
+    def lavar(self):
+        self.bebida = None
+        self.limpo = True
+        
+    def __str__(self):
+        if self.conteudo == 0:
+            return f'Um copo vazio de {self.tamanho}mL.'
+        else:
+            return f'Um copo de {self.tamanho}mL contendo {self.conteudo}mL de {self.bebida}.'
--- classes/recipiente.py
+++ classes/recipiente.py
@@ -1,1 +1,33 @@
-# Desenvolva sua classe Recipiente aqui.
+class Recipiente:
+    def __init__(self, tamanho: float, conteudo: float = 0, limpo: bool = True):
+        self.tamanho = tamanho
+        self.conteudo = conteudo
+        self.limpo = limpo
+
+    def esvaziar(self):
+        self.conteudo = 0
+
+    def esta_vazio(self):
+        if self.conteudo == 0:
+            return True
+        else:
+            return False
+
+    def lavar(self):
+        self.conteudo = 0
+        self.limpo = True
+    
+    def esta_limpo(self):
+        return self.limpo
+
+    def estado(self):
+        if self.limpo:
+            return 'limpo'
+        else:
+            return 'sujo'
+    
+    def sujar(self):
+        self.limpo = False
+
+    def __str__(self):
+        return f'O recipiente esta {self.estado()}'
